79c35aacd419daa94747d713394e3b21bcc7109d
Karthick <karthickthankyou@gmail.com>
2020-05-06 13:40:40 +0530

    Implemented security measures

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const path = require('path');
+
+const fileupload = require('express-fileupload');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 const profiles = require('./routes/profiles');
 const users = require('./routes/users');
@@ -16,8 +25,40 @@ const app = express();
 
 connectDB();
 app.use(express.json());
-app.use(express.json());
 app.use(cookieParser());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// File uploading
+app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/v1/profiles', profiles);
